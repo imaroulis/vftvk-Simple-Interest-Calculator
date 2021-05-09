7a329fa704a09ddda1f076f30bdb66c0a9208f27
Iakovos Maroulis <imaroulis@hotmail.com>
2021-05-09 10:45:56 +0000

    A few more changes

--- script.js
+++ script.js
@@ -12,9 +12,10 @@ function compute()
     p = document.getElementById("principal").value;
     if (principal <= 0) {
         alert("Enter a positive number");
+        document.getElementById("principal").focus();
         return;
     }
-    
+
     document.getElementById("result").innerHTML="If you deposit \<mark\>"+principal+"\</mark\>,\<br\>at an interest rate of \<mark\>"+rate+"%\</mark\>.\<br\>You will receive an amount of \<mark\>"+interest+"\</mark\>,\<br\>in the year \<mark\>"+year+"\</mark\>\<br\>"
     
 }
